c2bf39ff614949ce3e41ac4c1b73579f246e0bc1
jofrestrom <l.jofre@duocuc.cl>
2024-09-03 12:15:35 -0400

    actualizacion de inicio sesion e registro

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -11,5 +12,14 @@ export class RegisterPage implements OnInit {
 
   ngOnInit() {
   }
+  persona = new FormGroup({
 
+    rut: new FormControl(),
+    nombre: new FormControl(),
+    fecha_nacimiento: new FormControl(),
+    genero: new FormControl(),
+    tiene_equipo: new FormControl(),
+    nombre_equipo: new FormControl()
+
+  });
 }
